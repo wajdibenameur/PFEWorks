b0993149182c88ef96fb5dffc7fb2fbfb6669bdf
wajdi ben ameur <ben.ameur.wajdi@gmail.com>
2026-04-14 20:28:43 +0200

    Refactor repositories related to Zabbix

--- src/main/java/tn/iteam/repository/ZabbixMetricRepository.java
+++ src/main/java/tn/iteam/repository/ZabbixMetricRepository.java
@@ -3,5 +3,9 @@ package tn.iteam.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tn.iteam.domain.ZabbixMetric;
 
+import java.util.Optional;
+
 public interface ZabbixMetricRepository extends JpaRepository<ZabbixMetric, Long> {
+
+    Optional<ZabbixMetric> findByHostIdAndItemId(String hostId, String itemId);
 }
